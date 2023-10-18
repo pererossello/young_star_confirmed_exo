747af55d8d12b51bcb868f66090c5b5419e01289
pererossello <per.rossello@gmail.com>
2023-10-18 11:25:57 +0100

    changed_to_NASA_database

--- code/utils.py
+++ code/utils.py
@@ -19,3 +19,24 @@ def get_params(df):
     yerr = [yerr_lower, yerr_upper]
 
     return x, y, yerr
+
+
+
+def get_params_nasa(df):
+    x = np.arange(len(df))
+    y = np.array(df['st_age'])
+    idxs = ~np.isnan(y) # good indexes (no nan values)
+
+    # the low error bar is sometimes positive and sometimes negative (we should take the abs value)
+    yerr_lower = np.abs(np.array(df['st_ageerr2'])) # absolute value to avoid negative errorbars
+    # if yerr_lower is nan, set it to zero
+    yerr_lower[np.isnan(yerr_lower)] = 0
+    yerr_upper = np.abs(np.array(df['st_ageerr1'])) # absolute value to avoid negative errorbars
+    # if yerr_upper is nan, set it to zero
+    yerr_upper[np.isnan(yerr_upper)] = 0
+    
+    # grab only non-nan values (i.e. stars with age determined)
+    x, y, yerr_lower, yerr_upper = x[idxs], y[idxs], yerr_lower[idxs], yerr_upper[idxs]
+    yerr = [yerr_lower, yerr_upper]
+
+    return x, y, yerr
